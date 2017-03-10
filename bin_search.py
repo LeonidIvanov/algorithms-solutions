0b485b5aa6d9adc22cd3d2b894f60cf459006991
LeonidIvanov <git@github.com:LeonidIvanov/python0709.git>
2017-03-10 14:43:03 +0200

    Finished binary search vs. liniar

--- bin_search.py
+++ bin_search.py
@@ -1,22 +1,30 @@
-def bin_search(s, lst):
-    right = lst[int(len(lst) / 2):]
-    left = lst[:int(len(lst) / 2)]
-    print(left, right)
-    if right[0] == s:
-        print('Found it!!! Right!')
-        result = right[0]
-    elif left[-1] == s:
-        print('Found it!!! Left!')
-        result = left[-1]
-    elif right[0] < s:
-        bin_search(s, right)
-    elif left[-1] > s:
-        bin_search(s, left)
+def bin_search(s, lst, i=0):
+    if not lst:
+        return 0
     else:
-        print('Something went wrong!')
-    return result
+        right = lst[int(len(lst) / 2):]
+        left = lst[:int(len(lst) / 2)]
+        if right[0] < s:
+            i += 1
+            return bin_search(s, right, i)
+        if left[-1] > s:
+            i += 1
+            return bin_search(s, left, i)
+        if right[0] == s or left[-1] == s:
+            return '{} iteration(s)'.format(i)
+
+
+def lin_search(s, lst):
+    if not lst:
+        return 0
+    else:
+        for i in range(len(lst)):
+            if lst[i] == s:
+                return '{} iteration(s)'.format(i)
 
 if __name__ == '__main__':
-    mass = [x for x in range(0, 1000000)]
-    s = 2
+    import timeit
+    mass = [x for x in range(-1000000, 1000000)]
+    s = 999999
     print(bin_search(s, mass))
+    print(lin_search(s, mass))
